e65b79c79597f317e6aa7d9ace723ef70e6709aa
Alex Hirsch <w4rh4wk@bluephoenix.at>
2016-09-11 20:19:25 +0200

    Add lots of comments to hello.c

--- 01_hello_world/hello.c
+++ 01_hello_world/hello.c
@@ -1,7 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>		/* defines standard IO operations like `printf` */
+#include <stdlib.h>		/* defines EXIT_SUCCESS / EXIT_FAILURE */
 
+/* Main entry point of our program.
+ *
+ * It returns an exit code indication whether
+ * execution was successful or not. Your shell stores the last exit code
+ * received in `$?`. Try this:
+ *
+ *     $ make
+ *     $ ./hello
+ *     $ echo $?
+ *
+ * It should show answer with `0` which is equivalent to EXIT_SUCCESS (on
+ * Unix). Try changing EXIT_SUCCESS to a different number like 42 or 1337.
+ */
 int main(void) {
+	/* Print the string `Hello World` followed by a new-line. Another option
+	 * would be to use the `puts` function. */
 	printf("Hello World\n");
+
+	/* `main` returns the exit code of the program. */
 	return EXIT_SUCCESS;
 }
